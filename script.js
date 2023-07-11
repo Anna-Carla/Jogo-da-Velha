fb2fb157eda9829ad73b1cc82408811088192637
Anna-Carla <annacarlateixeira16@gmail.com>
2023-07-11 10:42:02 -0300

    resolvendo problema na board

---
+++ script.js
@@ -0,0 +1,42 @@
+const cellElements = document.querySelectorAll("[data-cell]")
+const board = document.querySelector("[data-board]");
+
+let isCircleTurn = false;
+
+const placeMark = (cell, classToAdd) => {
+    cell.classList.add(classToAdd);
+};
+
+const swapTurns = () => {
+    isCircleTurn = !isCircleTurn;
+
+    board.classList.remove('circle');
+    board.classList.remove('x');
+
+    if(isCircleTurn) {
+        board.classList.add ('circle');
+    } else {
+        board.classList.add ("x");
+    }
+
+};
+
+const handleClick = (e) => {
+    // Colocar a marca (X ou Circulo)
+    const cell = e.target;
+    const classToAdd = isCircleTurn ? "circle" : "x";
+
+    placeMark(cell, classToAdd);
+
+    //Verificar por vitória
+    //Verificar por empate
+    //Mudar simbolo
+
+    swapTurns();
+
+};
+
+for (const cell of cellElements) {
+    cell.addEventListener("click", handleClick, { once: true});
+}
+
